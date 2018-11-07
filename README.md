a3a70c7ae65ce893df1e23f51fb566b460d8b083
Witoslaw Koczewski <wi@koczewski.de>
2018-11-07 16:15:36 +0100

    bootstrap template

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # html-tools
+
 Clojure tools for creating html websites
+
+## License
+
+Copyright © 2018 Witoslaw Koczewski
+
+Distributed under the Eclipse Public License either version 1.0 or any later
+version.
---
+++ src/html_tools/api.clj
@@ -0,0 +1,61 @@
+(ns html-tools.api
+  (:require
+   [hiccup.page :as hiccup]
+   [hiccup.util :as util]))
+
+(def templates
+  {:bootstrap-cdn {:css-includes ["https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"]
+                   :js-includes ["https://code.jquery.com/jquery-3.3.1.slim.min.js"
+                                 "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+                                 "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"]}})
+
+
+(def escape util/escape-html)
+
+
+(defn- style-val
+  [v]
+  (cond
+    (keyword? v) (name v)
+    :else (str v)))
+
+(defn style
+  [css-prop-map]
+  (reduce
+   (fn [s [k v]]
+     (str s (name k) ": " (style-val v) "; "))
+   ""
+   css-prop-map))
+
+
+(defn css
+  [css]
+  (reduce
+   (fn [s [k v]]
+     (str s (name k) " {" (style v) "}\n"))
+   "\n"
+   css))
+
+(defn page-html
+  [config]
+  (hiccup/html5
+   [:head
+    [:meta {:charset "utf-8"}]
+    [:meta {:name "viewport"
+            :content "width=device-width, initial-scale=1, shrink-to-fit=no"}]
+    (for [uri (:css-includes config)]
+      (hiccup/include-css uri))
+    (if-let [css-map (:css config)]
+      [:style (css css-map)])
+    (if-let [title (:title config)]
+      [:title (escape title)])]
+   (-> [:body]
+       (into (:content config))
+       (into (map hiccup/include-js (:js-includes config)))
+       (conj [:script (:script config)]))))
+
+
+(defn write-page!
+  [page-config file]
+  (println "Writing" file)
+  (spit file (page-html page-config)))
