28a101f6ecaf667e7201da61d0e465f50cfdee6b
Witoslaw Koczewski <wi@koczewski.de>
2019-01-21 16:50:43 +0100

    static resource files

--- src/html_tools/server.clj
+++ src/html_tools/server.clj
@@ -1,6 +1,9 @@
 (ns html-tools.server
   (:require
-   [ring.middleware.reload :as reload]
+   [clojure.java.io :as io]
+
+   [ring.middleware.file :as ring-file]
+   [ring.middleware.reload :as ring-reload]
    [ring.adapter.jetty :as jetty]
 
    [html-tools.api :as html]))
@@ -9,26 +12,37 @@
 (def port 9500)
 
 
+(defn serve-file [website-config req resource]
+  (let [file (io/as-file (str "resources/" resource))]
+    (if (.exists file)
+      {:status 200
+       :content "exists"}
+      {:status 404})))
+
+
 (defn serve-request [website-config req]
   (let [uri (:uri req)
         uri (if (= "/" uri) "/index.html" uri)
         uri (.substring uri 1) ; cause uri always starts with '/'
         page-config (get-in website-config [:pages uri])]
-    (if page-config
+    (when page-config
       {:status 200
        :headers {"Content-Type" "text/html"}
-       :body (html/page-html (page-config req))}
-      {:status 404})))
+       :body (html/page-html (page-config req))})))
 
 
 (defn run-http-server
   [website-config]
   (println "\nStarting JETTY:      --> " (str "http://localhost:" port "/") "\n")
-  (jetty/run-jetty
-   (reload/wrap-reload
-    (fn [req]
-      (serve-request website-config req)))
-   {:port port}))
+  (-> (fn [req] (serve-request website-config req))
+      (ring-file/wrap-file "resources")
+      (ring-reload/wrap-reload)
+      (jetty/run-jetty {:port port})))
+  ;; (jetty/run-jetty
+  ;;  (reload/wrap-reload
+  ;;   (fn [req]
+  ;;     (serve-request website-config req)))
+  ;;  {:port port}))
 
 
 (defn demo-index-page [req]
--- src/html_tools/website.clj
+++ src/html_tools/website.clj
@@ -1,21 +1,49 @@
 (ns html-tools.website
   (:require
+   [clojure.java.io :as io]
+
    [html-tools.server :as server]
    [html-tools.api :as html-tools]))
 
 
 (defn generate-page-file [target file-name page-config]
   (let [file (str target "/" file-name)]
-    (println "  *" file-name)
+    (println "    *" file-name)
     (html-tools/write-page! page-config file)))
+
+(defn copy-file [target dir file-name]
+  (let [from-file (-> (str "resources/" dir file-name) io/as-file)
+        to-file (-> (str target "/" dir file-name) io/as-file)]
+    ;; (println "    *" (.getPath from-file) "->" (.getPath to-file))
+    (if (.isDirectory from-file)
+      (do
+        (.mkdir to-file)
+        (doall
+         (for [file (.listFiles from-file)]
+           (copy-file target (str dir (.getName from-file) "/") (.getName file)))))
+      (do
+        (println "    *" (str dir file-name))
+        (java.nio.file.Files/copy
+         (.toPath from-file)
+         (.toPath to-file)
+         (into-array java.nio.file.CopyOption
+                     [java.nio.file.StandardCopyOption/REPLACE_EXISTING]))))))
+
 
 
 (defn generate-files [target website-config]
-  (println "Generating website")
+  (println "generating website")
   (println "  target:" target)
+
+  (println "  generating pages")
   (doall
    (for [[file-name page-config] (get website-config :pages)]
-     (generate-page-file target file-name page-config))))
+     (generate-page-file target file-name page-config)))
+
+  (println "  copying resources")
+  (doall
+   (for [file (-> "resources" io/as-file .listFiles)]
+     (copy-file target "" (.getName file)))))
 
 
 (defn generate [target website-config]
