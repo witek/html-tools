45e86fa4e8e76f1bf3bababfd6fd00bb6b093cef
Witoslaw Koczewski <wi@koczewski.de>
2019-10-17 19:58:50 +0200

    google analytics

--- src/html_tools/snippets/google_analytics.clj
+++ src/html_tools/snippets/google_analytics.clj
@@ -11,6 +11,6 @@
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', '" tracking-id "')};
+gtag('config', '" tracking-id "');
 ")]
      "\n"]))
